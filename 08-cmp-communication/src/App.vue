aac6455542d8417e93482ce60a2212a70121b405
Pheak02 <saingsopheak02@gmail.com>
2024-07-10 23:54:53 +0700

    103. Demo: Adding Components & Connecting Them

--- 08-cmp-communication/src/App.vue
+++ 08-cmp-communication/src/App.vue
@@ -3,6 +3,7 @@
     <header>
       <h1>My Friends</h1>
     </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -49,6 +50,16 @@ export default {
       );
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
     },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false,
+      };
+      this.friends.push(newFriendContact);
+    },
   },
 };
 </script>
@@ -88,6 +99,15 @@ header {
   width: 90%;
   max-width: 40rem;
 }
+#app form {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
 #app h2 {
   font-size: 2rem;
   border-bottom: 4px solid #ccc;
@@ -108,5 +128,18 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
---
+++ 08-cmp-communication/src/components/NewFriend.vue
@@ -0,0 +1,54 @@
+<template>
+  <section>
+    <header>
+      <h1>Add Friend Contacts</h1>
+    </header>
+    <form @submit.prevent="submitData">
+      <input
+        type="text"
+        v-model="enteredName"
+        id="name"
+        placeholder="Enter name"
+      />
+      <input
+        type="text"
+        v-model="enteredPhone"
+        id="name"
+        placeholder="Enter phone"
+      />
+      <input
+        type="text"
+        v-model="enteredEmail"
+        id="name"
+        placeholder="Enter email"
+      />
+      <!-- <select name="favorite" id="fav">
+        <option value="true">Yes</option>
+        <option value="false">No</option>
+      </select> -->
+      <button>Add Contact</button>
+    </form>
+  </section>
+</template>
+<script>
+export default {
+  emits: ["add-contact"],
+  data() {
+    return {
+      enteredName: "",
+      enteredPhone: "",
+      enteredEmail: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit(
+        "add-contact",
+        this.enteredName,
+        this.enteredPhone,
+        this.enteredEmail
+      );
+    },
+  },
+};
+</script>
