76f20337243ec9b9123af27da7d6ad4e5c0d1be0
Sopheak Saing <sopheak.saing@khalibre.com>
2024-10-10 10:02:03 +0700

    224. Organizing your Store with Modules

--- 15_vuex/src/main.js
+++ 15_vuex/src/main.js
@@ -4,25 +4,23 @@ import { createStore } from 'vuex';
 
 import App from './App.vue';
 
-const store = createStore({
+const counterModule = {
     state(){
         return {
             counter: 0,
-            isLoggedIn: false
-        };
+        }
     },
     mutations: {
+
         increment(state) { //this state param will garantee by vuex
             state.counter = state.counter + 2;
         },
         increase(state, payload){
             state.counter = state.counter + payload.value;
         },
-        setAuth(state, payload){
-            state.isLoggedIn = payload.isAuth;
-        }  
     },
-    actions: { //to avoid async - consider good practice
+    actions:{
+
         increment(context) { //take context obj
             setTimeout(function() {
                 context.commit('increment');
@@ -32,15 +30,8 @@ const store = createStore({
             console.log(context)
             context.commit('increase', payload)
         },
-        login(context){
-            context.commit('setAuth', {isAuth: true})
-        },
-        logout(context){
-            context.commit('setAuth', {isAuth: false})
-        }
-
     },
-    getters: {
+    getters:{
         finalCounter(state) { // _ means i have to take it in order to work with the 2nd arg but i don't wanna work with it
             return state.counter * 2;
         },
@@ -54,6 +45,33 @@ const store = createStore({
             }
             return finalCounter;
         },
+    }
+}
+
+const store = createStore({
+    modules: {
+        numbers: counterModule
+    },
+    state(){
+        return {
+            isLoggedIn: false
+        };
+    },
+    mutations: {
+        setAuth(state, payload){
+            state.isLoggedIn = payload.isAuth;
+        }  
+    },
+    actions: { //to avoid async - consider good practice
+        login(context){
+            context.commit('setAuth', {isAuth: true})
+        },
+        logout(context){
+            context.commit('setAuth', {isAuth: false})
+        }
+
+    },
+    getters: {
         isUserAuth(state){
             return state.isLoggedIn;
         }
