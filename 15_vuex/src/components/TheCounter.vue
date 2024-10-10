f6f70846a41db8a9a0b9f82706495db117b98eab
Sopheak Saing <sopheak.saing@khalibre.com>
2024-10-10 09:12:17 +0700

    222. Using Mapper Helpers

--- 15_vuex/src/components/TheCounter.vue
+++ 15_vuex/src/components/TheCounter.vue
@@ -1,13 +1,16 @@
 <template>
-<h3>{{counter}}</h3>
+<h3>{{finalCounter}}</h3>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default{
     computed: {
-        counter() {
-            return this.$store.getters.finalCounter; //* just point at getter, not call/execute
-        }
+        // counter() {
+        //     return this.$store.getters.finalCounter; //* just point at getter, not call/execute
+        // }
+        ...mapGetters(['finalCounter'])
     }
 }
 </script>
